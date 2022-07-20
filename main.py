f243856b206cc961170b9b1210fa15b529ffc5da
Drallas <24792888+Drallas@users.noreply.github.com>
2022-07-20 12:06:18 +0200

    roll only between 1 and 6

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 
 import dice
 
-dice_num = dice.roll_dice()
+number_of_dices = 0
 
-print(dice_num)
+# main loop 
+if __name__ == "__main__":
+
+    number_of_dices = int(input("How many dices do you want to roll? "))
+
+    # check if the number of dices is vaid between 1 and 6
+    if number_of_dices < 1 or number_of_dices > 6:
+        print("You can only roll between 1 and 6 dices")
+        exit()
+
+    for roll in range(number_of_dices):
+        dice_num = dice.roll_dice()
+        print(f" Roll: {roll+1} - Number: {dice_num}\n")
+
