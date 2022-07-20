592722e052877c1c6a8fc2fd6546bbc3c0c52ed6
Drallas <24792888+Drallas@users.noreply.github.com>
2022-07-20 12:52:22 +0200

    add tests

---
+++ test.py
@@ -0,0 +1,23 @@
+from this import d
+import unittest
+
+import dice
+
+# Check tests via:
+# python3 -m unittest discover
+
+# Test 1: check if input outside of range is handled correctly and raises SystemExit and value between 1 and 6 is returned.
+
+
+class TestDice(unittest.TestCase):
+    def test_input(self):
+        assert dice.check_input(1) == 1
+        assert dice.check_input(6) == 6
+
+        with self.assertRaises(SystemExit):
+            check = [0, -1, 7, "a"]
+            for i in check:
+                dice.check_input(i)
+
+
+# Test 2: Check if roll_dice() returns a random number between 1 and 6.
