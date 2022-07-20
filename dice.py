7cda624ba838333d4d0be6b756d741fb2de6601d
Drallas <24792888+Drallas@users.noreply.github.com>
2022-07-20 12:51:59 +0200

    check input

--- dice.py
+++ dice.py
@@ -1,7 +1,14 @@
 import random
 
 
+def check_input(dices_roll_value):
+    '''Checks the input string and returns the number of dices to roll'''
+    if dices_roll_value < 1 or dices_roll_value > 6:
+        print("You can only roll between 1 and 6 dices")
+        raise SystemExit(1)
+    return dices_roll_value
+
+
 def roll_dice():
     '''Creates a random number between 1 and 6'''
     return random.randint(1, 6)
-
