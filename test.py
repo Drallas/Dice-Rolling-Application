062c06fb7829d3d0773b1c3e09e38e7b27bf4b2a
Drallas <24792888+Drallas@users.noreply.github.com>
2022-07-21 10:00:14 +0200

    clean

--- test.py
+++ test.py
@@ -5,22 +5,20 @@ import main
 
 # Check tests via: python3 -m unittest discover
 
-# Test 1: check if input outside of range is handled correctly and raises SystemExit and value between 1 and 6 is returned.
 
+class TestInput(unittest.TestCase):
+    '''check if input outside of range is handled correctly and raises SystemExit and value between 1 and 6 is returned.'''
 
-class TestDice(unittest.TestCase):
     def test_input(self):
-        assert main.check_input(1) == 1
-        assert main.check_input(6) == 6
-
         with self.assertRaises(SystemExit):
-            check = [0, -1, 7, "a"]
+            check = [-1, 0, 7, 1.1]
             for i in check:
                 main.check_input(i)
 
 
-# Test 2: Check if roll_dice() returns a random number between 1 and 6.
 class TestRollDice(unittest.TestCase):
+    '''Check if roll_dice() returns a random number between 1 and 6.'''
+
     def test_roll_dice(self):
         for i in range(100):
             assert 1 <= dice.roll_dice() <= 6
